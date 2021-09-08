a73cee26097b4d59d302280ad9c207a3ebaa4e83
Rakesh <rakesh22717@gmail.com>
2021-09-08 00:12:56 -0700

    fragments

--- app/src/main/java/com/example/herokotlin/MainActivity.kt
+++ app/src/main/java/com/example/herokotlin/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.herokotlin
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
+import androidx.fragment.app.*
 import com.example.herokotlin.fragments.MainFragment1
 import com.example.herokotlin.fragments.MainFragment2
 import com.example.herokotlin.presenters.MainPresenter
@@ -16,71 +13,37 @@ class MainActivity : MvpActivity<MainPresenter>(), MainView {
 
     lateinit var frag1: Button
     lateinit var frag2: Button
+    lateinit var fragmentManager: FragmentManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment1: Fragment = MainFragment1()
-        val fragment2: Fragment = MainFragment2()
-
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().apply {
-                add(R.id.fragment_container, fragment1)
-                add(R.id.fragment_container, fragment2)
-                hide(fragment2)
-                commit()
-                addToBackStack(null)
-            }
+            fragmentManager = supportFragmentManager
         }
 
         frag1 = findViewById(R.id.frag1)
         frag2 = findViewById(R.id.frag2)
 
-        App.addActivity(this)
-        App.removeActivity(this)
-
-
-
 
         frag1.setOnClickListener {
-
-            val fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
-
-            Log.d("test2x", "Fragment: $fragment")
-
-            supportFragmentManager.beginTransaction().apply {
-                hide(fragment2)
-                show(fragment1)
-                commit()
+            fragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<MainFragment1>(R.id.fragment_container, "fragment1")
             }
-
-
-//            supportFragmentManager.beginTransaction().apply {
-//                replace(R.id.fragment_container, fragment1)
-//                commit()
-//            }
         }
 
         frag2.setOnClickListener {
-
-            supportFragmentManager.beginTransaction().apply {
-                hide(fragment1)
-                show(fragment2)
-                commit()
+            fragmentManager.commit {
+                setReorderingAllowed(true)
+                replace<MainFragment2>(R.id.fragment_container, "fragment2")
             }
-
-//            supportFragmentManager.beginTransaction().apply {
-//                replace(R.id.fragment_container, fragment2)
-//                commit()
-//                addToBackStack(null)
-//            }
-
         }
     }
 
     override fun success(message: String) {
-       Log.d("test2x", "msg: $message")
+        Log.d("test2x", "msg: $message")
     }
 
     override fun fail(message: String) {
@@ -92,14 +55,22 @@ class MainActivity : MvpActivity<MainPresenter>(), MainView {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        if (supportFragmentManager.backStackEntryCount > 0) {
-            for (fragment in supportFragmentManager.fragments) {
-                if (fragment != null)
-                supportFragmentManager.beginTransaction().remove(fragment).detach(fragment).commit()
+        val arrayList = fragmentManager.fragments
+
+        for (item in arrayList) {
+            val fragment: Fragment? = fragmentManager.findFragmentByTag(item.tag)
+            if (fragment != null) {
+                fragmentManager.commit {
+                    remove(fragment)
+                }
             }
         }
-        finish()
+
+        if (arrayList.size == 0) {
+            finish()
+        }
+
+        Log.d("test2x", "list: $arrayList")
     }
 
 }
